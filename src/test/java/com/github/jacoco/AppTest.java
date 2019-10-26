228a5f0cc4be31c24da75614ccee72b10ff55644
Robin R <robinr@gmail.com>
2019-10-26 13:51:36 +0530

    added testAllUsers test in AppTest

--- src/test/java/com/github/jacoco/AppTest.java
+++ src/test/java/com/github/jacoco/AppTest.java
@@ -19,4 +19,12 @@ public class AppTest
 		Assert.assertEquals(response, "{\"isAlive\":true}");
 	}
 	
+	@Test
+	public void testAllUsers() throws ClientProtocolException, IOException {
+		String apiUrl = "http://192.168.40.138:8080/simplerestapp/v1/accounts/all";
+		String response = null;
+		response = app.httpClientTest(apiUrl);
+		Assert.assertEquals(response, "[{\"id\":1,\"name\":\"Ramu\",\"age\":24},{\"id\":2,\"name\":\"Suresh\",\"age\":38}]");
+	}
+	
 }
